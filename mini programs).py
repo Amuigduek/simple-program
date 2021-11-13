62003f8bf97bb8c9a5bee06d65a4167b88e84a85
Amuigduek <93607057+Amuigduek@users.noreply.github.com>
2021-11-13 12:44:15 +0530

    Add files via upload

---
+++ mini programs).py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello World")
+num=1000
+num1=2000
+print(num+num1)
+print(type(num+num1))
+results=(num<num1)
+print(results)
